d933b07199fc42b8ec1270eed5c7b380ebbeaf1a
henr1kas <h.sachmanovas@gmail.com>
2026-09-10 17:40:23 +0300

    Basic s5e8890 support

---
+++ scripts/s5e8890.py
@@ -0,0 +1,19 @@
+from soc import SoC, Image
+
+def soc_data():
+    return SoC(
+        signing_type=0,
+        odin=(
+            Image("cm.bin"),
+            Image("sboot.bin", split=(
+                Image("fwbl1.bin", stage="st1"),
+                Image("bl31.bin", update_header=True),
+                Image("bl2.bin"),
+                Image("pad.bin", stage=None),
+                Image("u-boot.bin"),
+                Image("el3_mon.bin"),
+                Image("tail.bin", stage=None),
+            )),
+            Image("HERO2LTE_EUR_OPEN_HIDDEN45M.pit")
+        )
+    )
